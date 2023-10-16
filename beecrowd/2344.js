22aef5709e92b46db0727bd7b578e9e2fd27334f
Apolo Moraes <vlogsapolo@gmail.com>
2023-10-16 16:25:28 -0300

    added exercise 2344

---
+++ beecrowd/2344.js
@@ -0,0 +1,13 @@
+const nota = parseInt(lines.shift());
+
+if (nota == 0) {
+  console.log("E");
+} else if (nota > 0 && nota <= 35) {
+  console.log("D");
+} else if (nota > 35 && nota <= 60) {
+  console.log("C");
+} else if (nota > 60 && nota <= 85) {
+  console.log("B");
+} else {
+  console.log("A");
+}
