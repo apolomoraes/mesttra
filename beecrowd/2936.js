98ab59b6f040a2c85b46e46864f00f398397732c
Apolo Moraes <vlogsapolo@gmail.com>
2023-10-17 16:59:13 -0300

    added exercise 2936

---
+++ beecrowd/2936.js
@@ -0,0 +1,14 @@
+const curupira = lines.shift();
+const boitata = lines.shift();
+const boto = lines.shift();
+const mapinguari = lines.shift();
+const lara = lines.shift();
+
+const curupiraResultado = curupira * 300;
+const boitataResultado = boitata * 1500;
+const botoResultado = boto * 600;
+const mapinguariResultado = mapinguari * 1000;
+const laraResultado = lara * 150;
+
+const resultadoFinal = curupiraResultado + boitataResultado + botoResultado + mapinguariResultado + laraResultado;
+console.log(resultadoFinal + 225);
