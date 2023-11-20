c5bd78cb08eeeb935d8b58ea119fdb19018b1ea4
Apolo Moraes <vlogsapolo@gmail.com>
2023-11-20 17:03:29 -0300

    added exercise 2780

--- beecrowd/2780.js
+++ beecrowd/2780.js
@@ -10,3 +10,28 @@ if (distancia <= 800) {
 }
 
 console.log(pontuacao);
+
+//--------------------------
+// Lê a distância D como entrada usando prompt
+// const distancia = lines.shift();
+
+// Define o objeto de mapeamento para os intervalos e pontuações
+// const mapeamento = {
+//   800: 1,
+//   1400: 2,
+//   2000: 3
+// };
+
+// Inicializa a pontuação como 0 (caso a distância seja maior que 2000)
+// let pontuacao = 0;
+
+// // Verifica em qual intervalo a distância se encontra
+// for (let intervalo in mapeamento) {
+//   if (distancia <= parseInt(intervalo)) {
+//     pontuacao = mapeamento[intervalo];
+//     break;
+//   }
+// }
+
+// // Imprime a pontuação
+// console.log(pontuacao);
